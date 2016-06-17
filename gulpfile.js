241fc6fdc91257df2d3c75870f94de7ea1537568
Nick Klunder <nklunder@gmail.com>
2016-06-17 10:58:26 -0700

    fix browserSync pug issue, create toggles page and CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,14 @@
 var gulp = require('gulp');
 var pug = require('gulp-pug');
 var postCSS = require('gulp-postcss');
-var cssNext = require('postcss-cssnext');
-var cssImport = require('postcss-partial-import');
-var rucksack = require('rucksack-css');
-var mqPacker = require('css-mqpacker');
-var cssNano = require('cssnano');
 var del = require('del');
 var browserSync = require('browser-sync').create();
 
+
 gulp.task('clean', function () {
-  return del('./build/**/*');
+  del('./build/**/*');
 });
+
 
 gulp.task('serve', function() {
   browserSync.init({
@@ -20,24 +17,31 @@ gulp.task('serve', function() {
     }
   });
 
-  gulp.watch("./src/css/**/*.css", ['style']);
-  gulp.watch('.src/js/**/*.js', ['scripts']);
-  gulp.watch("./src/pug/**/*.pug", ['views']);
+  gulp.watch("src/css/**/*.css", ['style']);
+  gulp.watch('src/js/**/*.js', ['scripts']);
+  gulp.watch("src/pug/**/*.pug", ['pug-watch']);
 });
+
 
 gulp.task('views', function () {
   return gulp.src('./src/pug/**/!(_)*.pug')
     .pipe(pug({ pretty: true }))
-    .pipe(gulp.dest('./build/'))
-    .pipe(browserSync.stream());
+    .pipe(gulp.dest('./build/'));
 });
+// Makes sure 'views' task completes before browser reload. 'reload' callback
+// has to be wrapped in function in order for it to work correctly
+//(https://stackoverflow.com/questions/29801070/gulp-browser-sync-only-works-once/30438407#30438407)
+gulp.task('pug-watch', ['views'], function () {
+  browserSync.reload()
+});
+
 
 gulp.task('style', function() {
   var processors = [
-    cssImport(),
-    cssNext(),
-    rucksack(),
-    mqPacker()
+    require('postcss-partial-import')(),
+    require('postcss-cssnext')(),
+    require('rucksack-css')(),
+    require('css-mqpacker')()
   ];
 
   return gulp.src('./src/css/style.css')
@@ -46,28 +50,26 @@ gulp.task('style', function() {
     .pipe(browserSync.stream());
 });
 
+
 gulp.task('scripts', function () {
   return gulp.src('./src/js/*.js')
     .pipe(gulp.dest('./build/js/'))
     .pipe(browserSync.stream());
 });
 
+
 gulp.task('images', function () {
   return gulp.src('./src/img/*.*')
     .pipe(gulp.dest('./build/img/'));
 });
+
 
 gulp.task('fonts', function () {
   return gulp.src('./src/fonts/*.*')
     .pipe(gulp.dest('./build/fonts/'));
 });
 
+
 gulp.task('default', [
-  'clean',
-  'views',
-  'style',
-  'scripts',
-  'images',
-  'fonts',
-  'serve'
+  'views', 'style', 'scripts', 'images', 'fonts', 'serve'
 ]);
