a49b9f4805f46794a0893b57794ba2a21715f9c2
Nick Klunder <nklunder@gmail.com>
2016-07-20 17:23:02 -0700

    fix some mobile nav weirdness, add page for snackbar demo

--- src/js/headroomConfig.js
+++ src/js/headroomConfig.js
@@ -1,6 +1,14 @@
-var header = document.querySelector('.global-nav')
-var headroom = new Headroom(header, {
+var nav = document.querySelector('.global-nav');
+var navMobile = document.querySelector('.global-nav--mobile');
+var headroom = new Headroom(nav, {
   "offset": 300,
   "tolerance": 5
 });
+
+var headroomMobile = new Headroom(navMobile, {
+  "offset": 200,
+  "tolerance": 5
+});
+
 headroom.init();
+headroomMobile.init();
